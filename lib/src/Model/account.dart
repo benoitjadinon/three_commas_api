25f635acad0451e26764ccefbde277f9912ed4f8
Benoit Jadinon <j@dinonbeno.it>
2019-04-30 00:10:26 +0200

    camelcase

--- lib/src/Model/account.dart
+++ lib/src/Model/account.dart
@@ -1,107 +1,105 @@
-import 'dart:convert';
-
-import 'package:json_annotation/json_annotation.dart';
-
 import 'package:decimal/decimal.dart';
+import 'package:json_annotation/json_annotation.dart';
 import 'package:three_commas_api/src/converters/decimal.dart';
 
 part 'account.g.dart';
 
 @JsonSerializable(nullable: false)
 class Account {
-  @JsonKey(name:"id")
-  int Id;
-  @JsonKey(name:"name")
-  String Name;
-  @JsonKey(name:"auto_balance_period", nullable: true)
-  int AutoBalancePeriod;
-  @JsonKey(name:"auto_balance_portfolio_id", nullable: true)
-  int AutoBalancePortfolioId;
-  @JsonKey(name:"auto_balance_currency_change_limit", nullable: true)
-  int AutoBalanceCurrencyChangeLimit;
-  @JsonKey(name:"autobalance_enabled")
-  bool AutoBalanceEnabled;
-  @JsonKey(name:"is_locked")
-  bool IsLocked;
-  @JsonKey(name:"smart_trading_supported")
-  bool SmartTradingSupported;
-  @JsonKey(name:"smart_selling_supported")
-  bool SmartSellingSupported;
-  @JsonKey(name:"available_for_trading")
-  Map<String,String> AvailableForTrading;
-  @JsonKey(name:"stats_supported")
-  bool StatsSupported;
-  @JsonKey(name:"trading_supported")
-  bool TradingSupported;
-  @JsonKey(name:"market_buy_supported")
-  bool MarketBuySupported;
-  @JsonKey(name:"conditional_buy_supported")
-  bool ConditionalBuySupported;
-  @JsonKey(name:"bots_allowed")
-  bool BotsAllowed;
-  @JsonKey(name:"created_at")
-  DateTime CreatedAt;
-  @JsonKey(name:"updated_at", nullable: true)
-  DateTime UpdatedAt;
-  @JsonKey(name:"last_auto_balance", nullable: true)
-  DateTime LastAutoBalance;
-  @JsonKey(name:"api_key")
-  String ApiKey;
-  @JsonKey(name:"auto_balance_error")
-  String AutoBalanceError;
-  @JsonKey(name:"lock_reason")
-  String LockReason;
-  @JsonKey(name:"btc_amount")
+  @JsonKey(name: "id")
+  int id;
+  @JsonKey(name: "name")
+  String name;
+  @JsonKey(name: "auto_balance_period", nullable: true)
+  int autoBalancePeriod;
+  @JsonKey(name: "auto_balance_portfolio_id", nullable: true)
+  int autoBalancePortfolioId;
+  @JsonKey(name: "auto_balance_currency_change_limit", nullable: true)
+  int autoBalanceCurrencyChangeLimit;
+  @JsonKey(name: "autobalance_enabled")
+  bool autoBalanceEnabled;
+  @JsonKey(name: "is_locked")
+  bool isLocked;
+  @JsonKey(name: "smart_trading_supported")
+  bool smartTradingSupported;
+  @JsonKey(name: "smart_selling_supported")
+  bool smartSellingSupported;
+  @JsonKey(name: "available_for_trading")
+  Map<String, String> availableForTrading;
+  @JsonKey(name: "stats_supported")
+  bool statsSupported;
+  @JsonKey(name: "trading_supported")
+  bool tradingSupported;
+  @JsonKey(name: "market_buy_supported")
+  bool marketBuySupported;
+  @JsonKey(name: "conditional_buy_supported")
+  bool conditionalBuySupported;
+  @JsonKey(name: "bots_allowed")
+  bool botsAllowed;
+  @JsonKey(name: "created_at")
+  DateTime createdAt;
+  @JsonKey(name: "updated_at", nullable: true)
+  DateTime updatedAt;
+  @JsonKey(name: "last_auto_balance", nullable: true)
+  DateTime lastAutoBalance;
+  @JsonKey(name: "api_key")
+  String apiKey;
+  @JsonKey(name: "auto_balance_error")
+  String autoBalanceError;
+  @JsonKey(name: "lock_reason")
+  String lockReason;
+  @JsonKey(name: "btc_amount")
   @DecimalConverter()
-  Decimal BtcAmount;
-  @JsonKey(name:"usd_amount")
+  Decimal btcAmount;
+  @JsonKey(name: "usd_amount")
   @DecimalConverter()
-  Decimal UsdAmount;
-  @JsonKey(name:"day_profit_btc")
+  Decimal usdAmount;
+  @JsonKey(name: "day_profit_btc")
   @DecimalConverter()
-  Decimal DailyProfitBtc;
-  @JsonKey(name:"day_profit_usd")
+  Decimal dailyProfitBtc;
+  @JsonKey(name: "day_profit_usd")
   @DecimalConverter()
-  Decimal DailyProfitUsd;
-  @JsonKey(name:"day_profit_btc_percentage")
+  Decimal dailyProfitUsd;
+  @JsonKey(name: "day_profit_btc_percentage")
   @DecimalConverter()
-  Decimal DailyProfitBtcPercentage;
-  @JsonKey(name:"day_profit_usd_percentage")
+  Decimal dailyProfitBtcPercentage;
+  @JsonKey(name: "day_profit_usd_percentage")
   @DecimalConverter()
-  Decimal DailyProfitUsdPercentage;
-  @JsonKey(name:"btc_profit")
+  Decimal dailyProfitUsdPercentage;
+  @JsonKey(name: "btc_profit")
   @DecimalConverter()
-  Decimal BtcProfit;
-  @JsonKey(name:"usd_profit")
+  Decimal btcProfit;
+  @JsonKey(name: "usd_profit")
   @DecimalConverter()
-  Decimal UsdProfit;
-  @JsonKey(name:"usd_profit_percentage")
+  Decimal usdProfit;
+  @JsonKey(name: "usd_profit_percentage")
   @DecimalConverter()
-  Decimal UsdProfitPercentage;
-  @JsonKey(name:"btc_profit_percentage")
+  Decimal usdProfitPercentage;
+  @JsonKey(name: "btc_profit_percentage")
   @DecimalConverter()
-  Decimal BtcProfitPercentage;
-  @JsonKey(name:"total_btc_profit")
+  Decimal btcProfitPercentage;
+  @JsonKey(name: "total_btc_profit")
   @DecimalConverter()
-  Decimal TotalBtcProfit;
-  @JsonKey(name:"total_usd_profit")
+  Decimal totalBtcProfit;
+  @JsonKey(name: "total_usd_profit")
   @DecimalConverter()
-  Decimal TotalUsdProfit;
-  @JsonKey(name:"pretty_display_type")
-  String PrettyDisplayType;
-  @JsonKey(name:"address")
-  String Address;
-  @JsonKey(name:"auto_balance_method", nullable: true)
+  Decimal totalUsdProfit;
+  @JsonKey(name: "pretty_display_type")
+  String prettyDisplayType;
+  @JsonKey(name: "address")
+  String address;
+  @JsonKey(name: "auto_balance_method", nullable: true)
   AccountBalanceMethod BalanceMethod;
 
   Account();
 
-  factory Account.fromJson(Map<String, dynamic> json) => _$AccountFromJson(json);
-  static List<Account> fromJsonList(List<dynamic> json) => json.map((i) => Account.fromJson(i)).toList(growable: false);
+  factory Account.fromJson(Map<String, dynamic> json) =>
+      _$AccountFromJson(json);
+
+  static List<Account> fromJsonList(List<dynamic> json) =>
+      json.map((i) => Account.fromJson(i)).toList(growable: false);
+
   Map<String, dynamic> toJson() => _$AccountToJson(this);
 }
 
-enum AccountBalanceMethod {
-  time,
-  currency_change
-}
+enum AccountBalanceMethod { time, currency_change }
--- lib/src/Model/account.g.dart
+++ lib/src/Model/account.g.dart
@@ -8,102 +8,102 @@ part of 'account.dart';
 
 Account _$AccountFromJson(Map<String, dynamic> json) {
   return Account()
-    ..Id = json['id'] as int
-    ..Name = json['name'] as String
-    ..AutoBalancePeriod = json['auto_balance_period'] as int
-    ..AutoBalancePortfolioId = json['auto_balance_portfolio_id'] as int
-    ..AutoBalanceCurrencyChangeLimit =
+    ..id = json['id'] as int
+    ..name = json['name'] as String
+    ..autoBalancePeriod = json['auto_balance_period'] as int
+    ..autoBalancePortfolioId = json['auto_balance_portfolio_id'] as int
+    ..autoBalanceCurrencyChangeLimit =
         json['auto_balance_currency_change_limit'] as int
-    ..AutoBalanceEnabled = json['autobalance_enabled'] as bool
-    ..IsLocked = json['is_locked'] as bool
-    ..SmartTradingSupported = json['smart_trading_supported'] as bool
-    ..SmartSellingSupported = json['smart_selling_supported'] as bool
-    ..AvailableForTrading =
+    ..autoBalanceEnabled = json['autobalance_enabled'] as bool
+    ..isLocked = json['is_locked'] as bool
+    ..smartTradingSupported = json['smart_trading_supported'] as bool
+    ..smartSellingSupported = json['smart_selling_supported'] as bool
+    ..availableForTrading =
         Map<String, String>.from(json['available_for_trading'] as Map)
-    ..StatsSupported = json['stats_supported'] as bool
-    ..TradingSupported = json['trading_supported'] as bool
-    ..MarketBuySupported = json['market_buy_supported'] as bool
-    ..ConditionalBuySupported = json['conditional_buy_supported'] as bool
-    ..BotsAllowed = json['bots_allowed'] as bool
-    ..CreatedAt = DateTime.parse(json['created_at'] as String)
-    ..UpdatedAt = json['updated_at'] == null
+    ..statsSupported = json['stats_supported'] as bool
+    ..tradingSupported = json['trading_supported'] as bool
+    ..marketBuySupported = json['market_buy_supported'] as bool
+    ..conditionalBuySupported = json['conditional_buy_supported'] as bool
+    ..botsAllowed = json['bots_allowed'] as bool
+    ..createdAt = DateTime.parse(json['created_at'] as String)
+    ..updatedAt = json['updated_at'] == null
         ? null
         : DateTime.parse(json['updated_at'] as String)
-    ..LastAutoBalance = json['last_auto_balance'] == null
+    ..lastAutoBalance = json['last_auto_balance'] == null
         ? null
         : DateTime.parse(json['last_auto_balance'] as String)
-    ..ApiKey = json['api_key'] as String
-    ..AutoBalanceError = json['auto_balance_error'] as String
-    ..LockReason = json['lock_reason'] as String
-    ..BtcAmount = const DecimalConverter().fromJson(json['btc_amount'])
-    ..UsdAmount = const DecimalConverter().fromJson(json['usd_amount'])
-    ..DailyProfitBtc = const DecimalConverter().fromJson(json['day_profit_btc'])
-    ..DailyProfitUsd = const DecimalConverter().fromJson(json['day_profit_usd'])
-    ..DailyProfitBtcPercentage =
+    ..apiKey = json['api_key'] as String
+    ..autoBalanceError = json['auto_balance_error'] as String
+    ..lockReason = json['lock_reason'] as String
+    ..btcAmount = const DecimalConverter().fromJson(json['btc_amount'])
+    ..usdAmount = const DecimalConverter().fromJson(json['usd_amount'])
+    ..dailyProfitBtc = const DecimalConverter().fromJson(json['day_profit_btc'])
+    ..dailyProfitUsd = const DecimalConverter().fromJson(json['day_profit_usd'])
+    ..dailyProfitBtcPercentage =
         const DecimalConverter().fromJson(json['day_profit_btc_percentage'])
-    ..DailyProfitUsdPercentage =
+    ..dailyProfitUsdPercentage =
         const DecimalConverter().fromJson(json['day_profit_usd_percentage'])
-    ..BtcProfit = const DecimalConverter().fromJson(json['btc_profit'])
-    ..UsdProfit = const DecimalConverter().fromJson(json['usd_profit'])
-    ..UsdProfitPercentage =
+    ..btcProfit = const DecimalConverter().fromJson(json['btc_profit'])
+    ..usdProfit = const DecimalConverter().fromJson(json['usd_profit'])
+    ..usdProfitPercentage =
         const DecimalConverter().fromJson(json['usd_profit_percentage'])
-    ..BtcProfitPercentage =
+    ..btcProfitPercentage =
         const DecimalConverter().fromJson(json['btc_profit_percentage'])
-    ..TotalBtcProfit =
+    ..totalBtcProfit =
         const DecimalConverter().fromJson(json['total_btc_profit'])
-    ..TotalUsdProfit =
+    ..totalUsdProfit =
         const DecimalConverter().fromJson(json['total_usd_profit'])
-    ..PrettyDisplayType = json['pretty_display_type'] as String
-    ..Address = json['address'] as String
+    ..prettyDisplayType = json['pretty_display_type'] as String
+    ..address = json['address'] as String
     ..BalanceMethod = _$enumDecodeNullable(
         _$AccountBalanceMethodEnumMap, json['auto_balance_method']);
 }
 
 Map<String, dynamic> _$AccountToJson(Account instance) => <String, dynamic>{
-      'id': instance.Id,
-      'name': instance.Name,
-      'auto_balance_period': instance.AutoBalancePeriod,
-      'auto_balance_portfolio_id': instance.AutoBalancePortfolioId,
+      'id': instance.id,
+      'name': instance.name,
+      'auto_balance_period': instance.autoBalancePeriod,
+      'auto_balance_portfolio_id': instance.autoBalancePortfolioId,
       'auto_balance_currency_change_limit':
-          instance.AutoBalanceCurrencyChangeLimit,
-      'autobalance_enabled': instance.AutoBalanceEnabled,
-      'is_locked': instance.IsLocked,
-      'smart_trading_supported': instance.SmartTradingSupported,
-      'smart_selling_supported': instance.SmartSellingSupported,
-      'available_for_trading': instance.AvailableForTrading,
-      'stats_supported': instance.StatsSupported,
-      'trading_supported': instance.TradingSupported,
-      'market_buy_supported': instance.MarketBuySupported,
-      'conditional_buy_supported': instance.ConditionalBuySupported,
-      'bots_allowed': instance.BotsAllowed,
-      'created_at': instance.CreatedAt.toIso8601String(),
-      'updated_at': instance.UpdatedAt?.toIso8601String(),
-      'last_auto_balance': instance.LastAutoBalance?.toIso8601String(),
-      'api_key': instance.ApiKey,
-      'auto_balance_error': instance.AutoBalanceError,
-      'lock_reason': instance.LockReason,
-      'btc_amount': const DecimalConverter().toJson(instance.BtcAmount),
-      'usd_amount': const DecimalConverter().toJson(instance.UsdAmount),
+          instance.autoBalanceCurrencyChangeLimit,
+      'autobalance_enabled': instance.autoBalanceEnabled,
+      'is_locked': instance.isLocked,
+      'smart_trading_supported': instance.smartTradingSupported,
+      'smart_selling_supported': instance.smartSellingSupported,
+      'available_for_trading': instance.availableForTrading,
+      'stats_supported': instance.statsSupported,
+      'trading_supported': instance.tradingSupported,
+      'market_buy_supported': instance.marketBuySupported,
+      'conditional_buy_supported': instance.conditionalBuySupported,
+      'bots_allowed': instance.botsAllowed,
+      'created_at': instance.createdAt.toIso8601String(),
+      'updated_at': instance.updatedAt?.toIso8601String(),
+      'last_auto_balance': instance.lastAutoBalance?.toIso8601String(),
+      'api_key': instance.apiKey,
+      'auto_balance_error': instance.autoBalanceError,
+      'lock_reason': instance.lockReason,
+      'btc_amount': const DecimalConverter().toJson(instance.btcAmount),
+      'usd_amount': const DecimalConverter().toJson(instance.usdAmount),
       'day_profit_btc':
-          const DecimalConverter().toJson(instance.DailyProfitBtc),
+          const DecimalConverter().toJson(instance.dailyProfitBtc),
       'day_profit_usd':
-          const DecimalConverter().toJson(instance.DailyProfitUsd),
+          const DecimalConverter().toJson(instance.dailyProfitUsd),
       'day_profit_btc_percentage':
-          const DecimalConverter().toJson(instance.DailyProfitBtcPercentage),
+          const DecimalConverter().toJson(instance.dailyProfitBtcPercentage),
       'day_profit_usd_percentage':
-          const DecimalConverter().toJson(instance.DailyProfitUsdPercentage),
-      'btc_profit': const DecimalConverter().toJson(instance.BtcProfit),
-      'usd_profit': const DecimalConverter().toJson(instance.UsdProfit),
+          const DecimalConverter().toJson(instance.dailyProfitUsdPercentage),
+      'btc_profit': const DecimalConverter().toJson(instance.btcProfit),
+      'usd_profit': const DecimalConverter().toJson(instance.usdProfit),
       'usd_profit_percentage':
-          const DecimalConverter().toJson(instance.UsdProfitPercentage),
+          const DecimalConverter().toJson(instance.usdProfitPercentage),
       'btc_profit_percentage':
-          const DecimalConverter().toJson(instance.BtcProfitPercentage),
+          const DecimalConverter().toJson(instance.btcProfitPercentage),
       'total_btc_profit':
-          const DecimalConverter().toJson(instance.TotalBtcProfit),
+          const DecimalConverter().toJson(instance.totalBtcProfit),
       'total_usd_profit':
-          const DecimalConverter().toJson(instance.TotalUsdProfit),
-      'pretty_display_type': instance.PrettyDisplayType,
-      'address': instance.Address,
+          const DecimalConverter().toJson(instance.totalUsdProfit),
+      'pretty_display_type': instance.prettyDisplayType,
+      'address': instance.address,
       'auto_balance_method':
           _$AccountBalanceMethodEnumMap[instance.BalanceMethod]
     };
